1447b67b688173d3a23a8ae81cb19eebb31d1b88
Juste_Marie <80101274+justinf006@users.noreply.github.com>
2025-12-10 15:57:36 -0800

    Update fitquest_db.sql

--- fitquest_db.sql
+++ fitquest_db.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `fitquest_db`
 --
-
+  CREATE DATABASE IF NOT EXISTS 'Fitquest_DB';
+  USE 'Fitquest_DB' ;
 -- --------------------------------------------------------
 
 --
